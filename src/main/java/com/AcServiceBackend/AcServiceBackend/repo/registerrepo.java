55d3ea4dfd2a4c639a8a67c253948155990fd31b
rahular2313 <rahularuchamy@gmail.com>
2023-06-27 11:39:05 +0530

    last

--- src/main/java/com/AcServiceBackend/AcServiceBackend/repo/registerrepo.java
+++ src/main/java/com/AcServiceBackend/AcServiceBackend/repo/registerrepo.java
@@ -1,10 +1,7 @@
 package com.AcServiceBackend.AcServiceBackend.repo;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.AcServiceBackend.AcServiceBackend.model.registermodel;
-
 import jakarta.transaction.Transactional;
 
 @Repository
